b3572bda64b52230ebeed390d500973ac578bb4f
SabinaGarayeva <sgarayeva13@gmail.com>
2024-12-03 14:59:17 +0400

    Product Detail page

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,10 +1,26 @@
+import Container from "@/app/components/Container";
+import { product } from "@/utils/product";
+import ProductDetail from "./ProductDetail";
+import ListRating from "./ListRating";
+
 interface IPrams {
   productId?: string;
 }
 
 const Product = ({ params }: { params: IPrams }) => {
-  console.log('params', params);
-  return <div>Product Page</div>;
+  console.log("params", params);
+
+  return (
+    <div className="p-8">
+      <Container>
+        <ProductDetail product={product} />
+        <div className="flex flex-col mt-20 gap-4">
+          <div>Add Rating</div>
+          <ListRating product={product} />
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default Product;
