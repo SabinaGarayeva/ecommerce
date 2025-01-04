89e9218662aacae650a78161bdfec62445c81150
SabinaGarayeva <sgarayeva13@gmail.com>
2025-01-04 23:01:55 +0400

    Clear Cart

--- hooks/useCart.tsx
+++ hooks/useCart.tsx
@@ -16,6 +16,7 @@ type CartContextType = {
   handleRemoveProductFromCart: (product: CartProductType) => void;
   handleCartQtyIncrease: (product: CartProductType) => void;
   handleCartQtyDecrease: (product: CartProductType) => void;
+  handleClearCart: () => void;
 };
 
 export const CartContext = createContext<CartContextType | null>(null);
@@ -123,6 +124,12 @@ export const CartContextProvider = (props: Props) => {
     [cartProducts]
   );
 
+  const handleClearCart = useCallback(() => {
+    setCartProducts(null);
+    setCartTotalQty(0);
+    localStorage.setItem("eShopCartItems", JSON.stringify(null));
+  }, [cartProducts]);
+
   const value = {
     cartTotalQty,
     cartProducts,
@@ -130,6 +137,7 @@ export const CartContextProvider = (props: Props) => {
     handleRemoveProductFromCart,
     handleCartQtyIncrease,
     handleCartQtyDecrease,
+    handleClearCart,
   };
 
   return <CartContext.Provider value={value} {...props} />;
