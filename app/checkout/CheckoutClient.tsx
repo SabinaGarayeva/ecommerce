699fcd90cb4120048de5cf1f02f93a2a6c9b8e1c
SabinaGarayeva <sgarayeva13@gmail.com>
2025-01-17 14:54:26 +0400

    10441

--- app/checkout/CheckoutClient.tsx
+++ app/checkout/CheckoutClient.tsx
@@ -2,15 +2,25 @@
 
 import { useCart } from "@/hooks/useCart";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Checkout from "./page";
 import { toast } from "react-hot-toast";
+import { StripeElementsOptions, loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "./CheckoutForm";
+import Button from "../components/Button";
+
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISAHBLE_KEY as string
+);
 
 const CheckoutClient = () => {
   const { cartProducts, paymentIntent, handleSetPaymentIntent } = useCart();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const [clientSecret, setClientSecret] = useState("");
+
+  const [paymentSuccess, setPaymentSuccess] = useState(false);
 
   const router = useRouter();
 
@@ -19,38 +29,73 @@ const CheckoutClient = () => {
 
   useEffect(() => {
     //create a paymentIntent as soon as the page loads
-   if (cartProducts) {
-    setLoading(true);
-    setError(false);
-    fetch("/api/create-payment-intent", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        items: cartProducts,
-        payment_intent_id: paymentIntent,
-      }),
-    })
-      .then((res) => {
-        setLoading(false);
-        if (res.status === 401) {
-          return router.push("/login");
-        }
-        return res.json();
+    if (cartProducts) {
+      setLoading(true);
+      setError(false);
+      fetch("/api/create-payment-intent", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          items: cartProducts,
+          payment_intent_id: paymentIntent,
+        }),
       })
-      .then((data) => {
-        setClientSecret(data.paymentIntent.client_secret);
-        handleSetPaymentIntent(data.paymentIntent.id);
-      })
-      .catch((error) => {
-        setError(true);
-        console.log("Error", error);
-        toast.error("Something went wrong");
-      });
-   }
+        .then((res) => {
+          setLoading(false);
+          if (res.status === 401) {
+            return router.push("/login");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setClientSecret(data.paymentIntent.client_secret);
+          handleSetPaymentIntent(data.paymentIntent.id);
+        })
+        .catch((error) => {
+          setError(true);
+          console.log("Error", error);
+          toast.error("Something went wrong");
+        });
+    }
   }, [cartProducts, paymentIntent]);
 
+  const options: StripeElementsOptions = {
+    clientSecret,
+    appearance: {
+      theme: "stripe",
+      labels: "floating",
+    },
+  };
+
+  const handleSetPaymentSuccess = useCallback((value: boolean) => {
+    setPaymentSuccess(value);
+  }, []);
+
   return (
-   <>Checkout</>
+    <div className="w-full">
+      {clientSecret && cartProducts && (
+        <Elements options={options} stripe={stripePromise}>
+          <CheckoutForm
+            clientSecret={clientSecret}
+            handleSetPaymentSuccess={handleSetPaymentSuccess}
+          />
+        </Elements>
+      )}
+
+      {loading && <div className="text-center">Loading Checkout...</div>}
+      {error && <div>Something went wrong...</div>}
+      {paymentSuccess && (
+        <div>
+          <div className="text-teal-500 text-center">Payment Success</div>
+          <div className="max-w-[220px] w-full">
+            <Button
+              label="View Your Orders"
+              onClick={() => router.push("/order")}
+            />
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
 
